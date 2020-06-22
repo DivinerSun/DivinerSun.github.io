ef71b374034604397527b5c66afec146a81ba7fb
Diviner <sqq_job@sina.com>
2020-06-22 12:09:28 +0800

    Site updated: 2020-06-22 12:09:27

---
+++ js/encryption.js
@@ -0,0 +1,10 @@
+function aes(imgUrl, pass) {
+    const passInput = document.getElementById("pass");
+    const btn = document.getElementById("btn_decrypt");
+    console.log(11111111, passInput, passInput.target.value, btn, pass);
+    return imgUrl;
+}
+
+function viewClick() {
+    console.log(22222222222222);
+}
